8c5581bd45a3e4e165380b2468050d8d47889e41
Patrik Florians <patrik.florians@usu.com>
2026-06-29 19:00:31 +0200

    add more plugins

---
+++ plugins/sprite-plugin/src/main/java/scape/editor/plugin/Plugin.kt
@@ -0,0 +1,17 @@
+package scape.editor.plugin
+
+@PluginDescriptor(name = "Vanilla 317 Sprite Plugin")
+class Plugin : IPlugin {
+
+    override fun applicationIcon(): String {
+        return "icons/icon.png"
+    }
+
+    override fun fxml(): String {
+        return "scene.fxml"
+    }
+
+    override fun stylesheets(): Array<String> {
+        return arrayOf("css/style.css")
+    }
+}
